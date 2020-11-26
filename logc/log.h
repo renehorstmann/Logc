1d33bc4bbee881bc4ba098dddcfad9568073538f
renehorstmann <r.horsty@gmail.com>
2020-11-26 10:27:23 +0100

    namespaced src to log_src_*_

--- logc/log.h
+++ logc/log.h
@@ -99,79 +99,79 @@ static struct {
     bool quiet;
     log_lock_function lock_function;
     void *used_data;
-} local;
+} log_src_local_;
 
 
-static const char *level_names[] = {
+static const char *log_src_level_names_[] = {
         "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "WTF"
 };
 
 #ifndef LOG_DO_NOT_USE_COLOR
-static const char *level_colors[] = {
+static const char *log_src_level_colors_[] = {
         "\x1b[94m", "\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m", "\x1b[35m"
 };
 #endif
 
 
-static void lock() {
-    if (local.lock_function) {
-        local.lock_function(local.used_data, true);
+static void log_src_lock_() {
+    if (log_src_local_.lock_function) {
+        log_src_local_.lock_function(log_src_local_.used_data, true);
     }
 }
 
 
-static void unlock() {
-    if (local.lock_function) {
-        local.lock_function(local.used_data, false);
+static void log_src_unlock_() {
+    if (log_src_local_.lock_function) {
+        log_src_local_.lock_function(log_src_local_.used_data, false);
     }
 }
 
 
 void LogcNamespace(LOGC_NAMESPACE, _set_log_file)(FILE *file) {
-    local.log_file = file;
+    log_src_local_.log_file = file;
 }
 
 void LogcNamespace(LOGC_NAMESPACE, _set_min_level)(enum log_level level) {
-    local.level = level;
+    log_src_local_.level = level;
 }
 
 void LogcNamespace(LOGC_NAMESPACE, _set_quiet)(bool set) {
-    local.quiet = set;
+    log_src_local_.quiet = set;
 }
 
 void LogcNamespace(LOGC_NAMESPACE, _set_locking_function)(log_lock_function fun) {
-    local.lock_function = fun;
+    log_src_local_.lock_function = fun;
 }
 
 void LogcNamespace(LOGC_NAMESPACE, _set_locking_function_user_data)(void *user_data) {
-    local.used_data = user_data;
+    log_src_local_.used_data = user_data;
 }
 
 
 void LogcNamespace(LOGC_NAMESPACE, _base_)(enum log_level level, const char *leading_text,
                                               const char *file, int line, const char *format, ...) {
-    if (level < local.level) {
+    if (level < log_src_local_.level) {
         return;
     }
 
     /* Acquire lock_function */
-    lock();
+    log_src_lock_();
 
     /* Get current time */
     time_t t = time(NULL);
     struct tm *lt = localtime(&t);
 
     /* Log to LOGC_DEFAULT_FILE (stdout) */
-    if (!local.quiet) {
+    if (!log_src_local_.quiet) {
         va_list args;
         char buf[16];
         buf[strftime(buf, sizeof(buf), "%H:%M:%S", lt)] = '\0';
 #ifdef LOG_DO_NOT_USE_COLOR
         fprintf(LOGC_DEFAULT_FILE, "%s %-5s %s:%d: %s",
-                buf, level_names[level], file, line, leading_text);
+                buf, log_src_level_names_[level], file, line, leading_text);
 #else
         fprintf(LOGC_DEFAULT_FILE, LOGC_LEADING_TEXT "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m %s",
-                buf, level_colors[level], level_names[level], file, line, leading_text);
+                buf, log_src_level_colors_[level], log_src_level_names_[level], file, line, leading_text);
 #endif
         va_start(args, format);
         vfprintf(LOGC_DEFAULT_FILE, format, args);
@@ -181,21 +181,21 @@ void LogcNamespace(LOGC_NAMESPACE, _base_)(enum log_level level, const char *lea
     }
 
     /* Log to file */
-    if (local.log_file) {
+    if (log_src_local_.log_file) {
         va_list args;
         char buf[32];
         buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", lt)] = '\0';
-        fprintf(local.log_file, LOGC_LEADING_TEXT "%s %-5s %s:%d: %s",
-                buf, level_names[level], file, line, leading_text);
+        fprintf(log_src_local_.log_file, LOGC_LEADING_TEXT "%s %-5s %s:%d: %s",
+                buf, log_src_level_names_[level], file, line, leading_text);
         va_start(args, format);
-        vfprintf(local.log_file, format, args);
+        vfprintf(log_src_local_.log_file, format, args);
         va_end(args);
-        fprintf(local.log_file, "\n");
-        fflush(local.log_file);
+        fprintf(log_src_local_.log_file, "\n");
+        fflush(log_src_local_.log_file);
     }
 
     /* Release lock_function */
-    unlock();
+    log_src_unlock_();
 }
 
 #endif
